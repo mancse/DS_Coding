dc77a194a2510a222c580a21e6229afffe4f7419
unknown <mancse@gmail.com>
2024-12-14 12:38:47 +0530

    change

--- src/TwoPointers/MaximumSlidingWindows.java
+++ src/TwoPointers/MaximumSlidingWindows.java
@@ -1,4 +1,8 @@
 package TwoPointers;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Given an array arr of size N and an integer K, the task is to find the maximum for
  * each and every contiguous subarray of size K.
@@ -28,29 +32,29 @@ package TwoPointers;
 
 public class MaximumSlidingWindows 
 {
-	 public static int[] maxSlidingWindow(int[] nums, int k) 
-	 {
-		int [] result = new int[nums.length - k +1];
-	    if (nums.length == 0)
-	    	return result;
-	    
-	    int right = k -1;
-	    
-	    int left = 0;
-	    int r=0;
-	    while(right < nums.length)
-	    {
-	        left = right -(k -1);
-	    	int max = Integer.MIN_VALUE;
-	    	while (left <= right)
-	    	{
-	    		max = Math.max(max, nums[left++]);
-	    	}
-	    	result[r++] = max;
-	    	right++;
-	    }
-	    return result;
-	 }
+	public static int[] maxSlidingWindow(int[] nums, int k) {
+		Deque<Integer> dq = new LinkedList<Integer>();
+		int[] res = new int[nums.length -k + 1];
+		int j = 0;
+		for (int i=0; i<nums.length; i++){
+			//Check if queue first index is out of the window then remove the first element from the queue.
+			if (!dq.isEmpty() && (dq.peekFirst() < (i - k + 1))){
+				dq.pollFirst();
+			}
+
+			//Always ensure that queue contains elements in decreseasing order. So, if queue last element is less than current index value of nums array then remove elements from queue last.
+			while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+				dq.pollLast();
+			}
+			dq.offerLast(i);
+
+			//Once window size reaches k then keep storing the queue first element as answer which ensures that highest value of each window is achieved.
+			if (i >= k-1){
+				res[i-k+1] = nums[dq.peekFirst()];
+			}
+		}
+		return res;
+	}
 	 
 	 public static void main(String args[])
 	 {
