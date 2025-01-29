85cc71a227f991872848372390496239058d9bad
Manoj Kumar <mancse@gmail.com>
2025-01-29 09:27:49 +0530

    Update LongestPalindromicSubsequence.java

--- src/DP/LongestPalindromicSubsequence.java
+++ src/DP/LongestPalindromicSubsequence.java
@@ -1,62 +1,60 @@
-package DP;
-/**
+/*
+*       b   b   b   a   b
 *
-*         c   b   b   d
-*   		  
-*     c   1   1   2   2
-*	  
-*	  b   0   1   2   2
-*	  
-*	  b   0   0   1   1 
-*	  
-*	  d   0   0   0   1
-* @author Manoj.K
+*   b   1   2   3   3   4
 *
+*   b       1   2   2   3
+*
+*   b           1   1   3
+*
+*   a               1   1
+*
+*   b                   1
+dp[i][j] represents the length of the longest palindromic subsequence in the substring s[i:j] (i.e., from index i to j).
+
+If s.charAt(i) and s.charAt(j) are equal:
+We can extend the palindromic subsequence found in s[i+1:j-1] by adding s[i] and s[j] on both ends.
+The new palindromic subsequence length is
+     dp[i][j]=2+dp[i+1][j−1]
+Why +2? Because s[i] and s[j] themselves contribute 2 characters to the length.
+
+If s.charAt(i) and s.charAt(j) are not equal:
+    We must discard one of the characters (s[i] or s[j]) and take the best possible LPS from either:
+dp[i+1][j] (ignoring s[i])
+dp[i][j-1] (ignoring s[j])
 */
-public class LongestPalindromicSubsequence 
-{
-	public static int longestPalindromeSubseq(String s) 
-    {
-        int dp[][] = new int[s.length()][s.length()];
-        int result =0;
-        for (int g=0; g < dp[0].length; g++)
-        {
-            for (int i=0, j=g; j < dp[0].length; i++, j++)
-            {
-                if (g == 0)
-                {
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int length = s.length();
+        int[][] dp = new int[length][length];
+        int res = 0;
+
+        for (int d=0; d<length; d++){
+            for (int i=0, j=d; j<length; j++,i++){
+                if (d == 0){
                     dp[i][j] = 1;
                 }
-                else if (g == 1)
-                {
-                    if (s.charAt(i) == s.charAt(j))
-                    {
+                else if(d==1){
+                    if (s.charAt(i) == s.charAt(j)){
                         dp[i][j] = 2;
                     }
-                    else
-                    {
+                    else{
                         dp[i][j] = 1;
                     }
                 }
-                else
-                {
-                    if (s.charAt(i) == s.charAt(j))
-                    {
+                else{
+                    if(s.charAt(i) == s.charAt(j) && i+1 < length && j-1 >= 0){
                         dp[i][j] = 2 + dp[i+1][j-1];
                     }
-                    else
-                    {
-                        dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+                    else{
+                        if (i+1 < length && j-1 >=0){
+                            dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+                        }
                     }
                 }
-                result = Math.max(result,dp[i][j]);
+                res = Math.max(res,dp[i][j]);
             }
         }
-        return result;
+        return res;
     }
-	
-	public static void main(String args[]){
-		int res = longestPalindromeSubseq("cbbdbc");
-		System.out.println("Longest palindrom subsequence: "+res);
-	}
 }
