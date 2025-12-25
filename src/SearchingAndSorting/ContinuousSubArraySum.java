2cc8fd7c938d10936dba18a0ca8dcd07335e3fb2
Manoj Kumar <mancse@gmail.com>
2025-12-25 20:19:17 +0530

    Update ContinuousSubArraySum.java

--- src/SearchingAndSorting/ContinuousSubArraySum.java
+++ src/SearchingAndSorting/ContinuousSubArraySum.java
@@ -1,1 +1,34 @@
+/**
+** Problem: Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+A good subarray is a subarray where:
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.
+https://leetcode.com/problems/continuous-subarray-sum/description/
+Solution: 
+  Idea: To check (prefixSum % k) repeats, if yes then Then the subarray between those indices has sum divisible by k.
 
+ */
+class ContinuousSubArraySum {
+    public boolean checkSubarraySum(int[] nums, int k) {
+       //Map to store rem (prefixSum % k) vs index
+       Map<Integer,Integer> map = new HashMap<>();
+       map.put(0,-1);
+       int sum=0;
+       for (int i=0; i<nums.length; i++){
+           sum += nums[i];
+           int rem = sum % k;
+           
+           //Check at least two length of continuous array
+           if (map.containsKey(rem)){
+               if (i - map.get(rem) >= 2){
+                return true;
+               }
+           }
+           //Insert if rem is not present. 
+           else{
+              map.put(rem,i);
+           }
+       }
+    return false;
+    }
+}
