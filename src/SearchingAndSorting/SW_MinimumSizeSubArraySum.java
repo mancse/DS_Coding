18192831f737ba019d8c8249e6ffa115b66f3ea8
Manoj Kumar <mancse@gmail.com>
2025-12-25 23:21:39 +0530

    Update SW_MinimumSizeSubArraySum.java

--- src/SearchingAndSorting/SW_MinimumSizeSubArraySum.java
+++ src/SearchingAndSorting/SW_MinimumSizeSubArraySum.java
@@ -1,1 +1,22 @@
+/**
+** Problem: https://leetcode.com/problems/minimum-size-subarray-sum/description/
+*/
 
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0; 
+        int right = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        while(right < nums.length){
+            if (sum < target){
+                sum += nums[right++];
+            }
+            while(sum >= target){
+                min = Math.min(min, (right - left));
+                sum -= nums[left++];
+            }
+        }
+        return min != Integer.MAX_VALUE ? min : 0;
+    }
+}
