f7608c05d0283870d45be2e4d9b0d6a2fee71ef7
Manoj Kumar <mancse@gmail.com>
2026-01-02 21:51:24 +0530

    Update BasicCalculator1.java

--- src/Stack/BasicCalculator1.java
+++ src/Stack/BasicCalculator1.java
@@ -1,1 +1,51 @@
+/**
+** Problem: https://leetcode.com/problems/basic-calculator/
+** 
+*/
+class Solution {
+    public int calculate(String s) {
+        int sign = 1;
+        int num = 0;
+        int res = 0;
+        Stack<Integer> st = new Stack<>();
 
+        for (int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if (ch != ' '){
+                if (Character.isDigit(ch)){
+                    num = num * 10 + ch - '0';
+                }
+
+                if (ch == '+'){
+                    res = res + sign * num;
+                    num = 0;
+                    sign = 1;
+                }
+                
+                else if (ch == '-'){
+                    res = res + sign * num;
+                    num = 0; 
+                    sign = -1;
+                }
+
+                else if (ch == '('){
+                    st.push(res);
+                    st.push(sign);
+                    num = 0;
+                    res = 0;
+                    sign = 1;
+                }
+
+                else if (ch == ')'){
+                    res = res + num * sign;
+                    int prevSign = st.pop();
+                    int prevRes = st.pop();
+                    res = prevRes + res * prevSign;
+                    num = 0;
+                }
+            }
+        }
+        // Finally at end of string
+        return res + num * sign;
+    }
+}
